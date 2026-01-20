2ce29c232f1700330803e83870029b8a64f7dfb5
seein <huangha607@naver.com>
2026-01-20 21:21:39 +0900

    setting project

---
+++ src/main/java/com/seein/SEE_INSIGHT/SeeInsightApplication.java
@@ -0,0 +1,13 @@
+package com.seein.SEE_INSIGHT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seein/SEE_INSIGHT/SeeInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seein.SEE_INSIGHT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
