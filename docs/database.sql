070495858ee68012a6e67e95e4d3e8333d400ca9
Leeseein <huangha607@naver.com>
2026-02-18 20:44:18 +0900

    docs: 테이블 및 API 명세 (v1)

--- docs/database.sql
+++ docs/database.sql
@@ -1,47 +1,50 @@
 CREATE TABLE `member` (
-`member_id` int PRIMARY KEY AUTO_INCREMENT,
-`email` varchar(255) UNIQUE NOT NULL COMMENT '로그인 ID 및 발송 주소',
-`nickname` varchar(255),
-`is_active` boolean DEFAULT true COMMENT '탈퇴 여부',
-`created_at` timestamp DEFAULT (now()),
-`membership` varchar(255) COMMENT 'normal, premium'
+    `member_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) UNIQUE NOT NULL COMMENT '로그인 ID 및 발송 주소',
+    `nickname` varchar(255),
+    `membership` ENUM('NORMAL', 'PREMIUM') COMMENT '등급',
+    `provider` varchar(255) COMMENT 'google/naver',
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP COMMENT '탈퇴 시간'
 );
 
 CREATE TABLE `keyword` (
-`keyword_id` int PRIMARY KEY AUTO_INCREMENT,
-`keyword` varchar(255) UNIQUE NOT NULL COMMENT '키워드 (예: Java, 주식)',
-`created_at` timestamp DEFAULT (now())
+   `keyword_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   `keyword` varchar(255) UNIQUE NOT NULL COMMENT '키워드 (예: Java, 주식)',
+   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `subscription` (
-`subscription_id` int PRIMARY KEY AUTO_INCREMENT,
-`member_id` int,
-`keyword_id` int,
-`is_active` boolean DEFAULT true COMMENT '알림 설정',
-`notification_at` varchar(255) COMMENT '발송 설정 시간',
-`created_at` timestamp DEFAULT (now())
+    `subscription_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `member_id` int NOT NULL,
+    `keyword_id` int NOT NULL,
+    `is_active` boolean DEFAULT true COMMENT '알림설정',
+    `notification_time` varchar(255) COMMENT '발송 설정 시간',
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `news_card` (
-`news_id` int PRIMARY KEY AUTO_INCREMENT,
-`keyword_id` int,
-`summary_content` text COMMENT 'AI가 요약한 뉴스 본문',
-`source_link` varchar(255) COMMENT '뉴스 원문 링크',
-`created_date` date NOT NULL COMMENT '파티셔닝 및 조회 기준일',
-`created_at` timestamp DEFAULT (now())
+     `news_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     `keyword_id` int NOT NULL,
+     `summary_content` text COMMENT 'AI가 요약한 뉴스 본문',
+     `source_link` varchar(2048) COMMENT '뉴스 원문 링크',
+     `created_date` date NOT NULL COMMENT '파티셔닝 및 조회 기준일'
 );
 
 CREATE TABLE `send_log` (
-`log_id` int PRIMARY KEY AUTO_INCREMENT,
-`member_id` int,
-`sent_at` datetime DEFAULT (now()),
-`status` varchar(255) COMMENT 'SUCCESS / FAIL',
-`fail_reason` text COMMENT '실패 시 에러 메시지 저장'
+    `log_id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `subscription_id` int NOT NULL COMMENT '구독(키워드) 구분',
+    `sent_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `status` ENUM('SUCCESS', 'FAIL') COMMENT '발송 결과',
+    `issue_date` datetime COMMENT '발행 날짜',
+    `fail_reason` text COMMENT '실패 시 에러 메시지 저장'
 );
 
 CREATE UNIQUE INDEX `subscription_index_0` ON `subscription` (`member_id`, `keyword_id`);
 
-CREATE INDEX `news_card_index_1` ON `news_card` (`keyword_id`, `created_date`);
+CREATE UNIQUE INDEX `news_card_index_1` ON `news_card` (`created_date`, `keyword_id`);
 
 ALTER TABLE `subscription` ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
 
@@ -49,4 +52,4 @@ ALTER TABLE `subscription` ADD FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (
 
 ALTER TABLE `news_card` ADD FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (`keyword_id`);
 
-ALTER TABLE `send_log` ADD FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+ALTER TABLE `send_log` ADD FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`subscription_id`);
